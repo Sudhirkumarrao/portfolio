01b92e79e2fd9efe2bda1153f0fa88f0223d8621
sallada <sallada@cisco.com>
2024-08-24 21:43:58 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Scroll Animation
+///////////////////////////////////////////////////////////
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const targetEl = document.querySelector(href);
+      targetEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky Header
+///////////////////////////////////////////////////////////
+const sectionHeroEl = document.querySelector(".about-section");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const entry = entries[0];
+    console.log(entry);
+    if (entry.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+
+    if (entry.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-48px",
+  }
+);
+
+observer.observe(sectionHeroEl);
